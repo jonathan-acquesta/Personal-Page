e65d22502b03568f96d0d20430d1a2f217bc0883
Jonathan Caravaggio Acquesta <jonathan.acquesta@hotmail.com>
2020-04-21 02:28:46 -0300

    The first step to create multi language stratucture

--- personal-page/src/App.vue
+++ personal-page/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <div class="nav-bar"></div>
+    <div class="nav-bar">
+      <img class="language" src="./assets/logo.png" title="portuguÊs" />
+      <img class="language" src="./assets/logo.png" title="inglês" />
+    </div>
     <v-content>
       <Home />
     </v-content>
@@ -28,6 +31,13 @@ export default {
     background: linear-gradient(-90deg, rgb(144, 75, 255), rgb(15, 138, 253));
     height: 60px;
     margin-bottom: 15px;
+}
+
+.language{
+  width: 48px;
+  margin-left: 3px;
+  margin-right: 3px;
+  margin-top: 3px;
 }
 
 </style>
