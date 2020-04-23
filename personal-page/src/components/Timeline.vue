111641dc06c6407fff54b1f0dba3cdd6c2f63930
Jonathan Caravaggio Acquesta <jonathan.acquesta@hotmail.com>
2020-04-23 12:53:00 -0300

    new feature link with certificate

--- personal-page/src/components/Timeline.vue
+++ personal-page/src/components/Timeline.vue
@@ -2,6 +2,8 @@
 
 
   <div class="timeLine">
+
+
       <v-timeline class="timelineApp" :dense="$vuetify.breakpoint.smAndDown" :reverse="!$vuetify.breakpoint.smAndDown" :left="$vuetify.breakpoint.smAndDown">
         <v-timeline-item v-if="history.category.enable"
           v-for="(history, i) in histories"
@@ -20,25 +22,52 @@
               v-text="formatePeriod(history.date)"
             ></span>
           </template>
-          <div class="py-4">
-            <span style="opacity:0.6" v-if="$vuetify.breakpoint.smAndDown"
-              :class="`headline font-weight-bold ${history.category.color}--text`"
-              v-text="formatePeriod(history.date)"
-            ></span>
-            <br>
+          <v-card class="elevation-2">
+          <div class="py-4 card">
+
+            
+           
             <span
               :class="`headline font-weight-bold ${history.category.color}--text`"
               v-text="history.category.name[culture]"
             ></span>
             <h2 :class="`headline font-weight-light mb-4 ${history.category.color}--text`">
               {{ history.title[culture] }}</h2>
-            <div>
+              <div class="descriptionFull">
+            <div class="description">
               {{ history.description[culture] }}
             </div>
             <div>
+              <v-img class="descriptionImage"  height="110"  width="110" :src="history.image"></v-img>
+
+            </div>
+            </div>
+         
+            <div>
               <span class="tags" v-for="(tag, index) in history.tags" :key="index" >{{ tag[culture] }}</span>
             </div>
+            <br>
+               <div class="links">
+              <span  >Link: </span>
+              <a class="linkShow" v-on:click="overlay = true;">{{history.link.description[culture]}} </a>
+              
+               <v-overlay class="overlay" :value="overlay">
+      <v-btn
+        icon
+        @click="overlay = false"
+      >
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <v-img class="descriptionImageLink"  :src="history.link.image"></v-img>
+    </v-overlay>
+
+            </div>
+            <span style="opacity:0.8" v-if="$vuetify.breakpoint.smAndDown"
+              :class="`headline font-weight-bold ${history.category.color}--text`"
+              v-text="formatePeriod(history.date)"
+            ></span>
           </div>
+          </v-card>
         </v-timeline-item>
       </v-timeline>
   </div>
@@ -60,6 +89,7 @@
       right: false,
       left: false,
       small: false,
+      overlay: false,
       valueDeterminate: 50
     };
   },
@@ -69,6 +99,7 @@
     }
   },
   mounted() {
+    /*
     this.histories.push({
       date: new Date("2020-07-20 00:33:00"),
       category: this.$store.state.categoryType.professionalExperience,
@@ -218,6 +249,26 @@
       image: "./assets/thoughts.ico",
       tags: [this.$store.state.tags.scrum, this.$store.state.tags.kanban]
     });
+    */
+
+   this.histories.push({
+      date: new Date("2019-02-02"),
+      category: this.$store.state.categoryType.certifications,
+      title: {
+        "pt-BR": "Scrum.org - PSM 1",
+        "en-US": "Scrum.org - PSM 1",
+      },
+      description: {
+        "pt-BR": "A certificação Professional Scrum Master foi um momento importante para formalização do conhecimento que adquiri durante toda a minha carreira profissional. Eu tive a oportunidade de trabalhar com SCRUM desde o inicio dos anos 2010 e esse framework agregou em muito nos bons resultados que venho obtendo.",
+        "en-US": "The Professional Scrum Master certification was an important moment to formalize the knowledge I acquired throughout my professional career. I had the opportunity to work with SCRUM since the beginning of the 2010s and this framework added a lot to the good results I have been getting.",
+      },
+      image: require('@/assets/psmi.png'),
+      link: {
+        image:   require('@/assets/certificatepsm1.jpg'),
+        description:{ "pt-BR": "Certificado", "en-US": "Certificate"}
+      },
+      tags: [this.$store.state.tags.scrum]
+    });
 
     this.orderByDates();
     
@@ -239,8 +290,8 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.timeLine{
-  text-align:center;
+.card{
+  margin-left: 10px;
 }
 
 .timelineApp{
@@ -265,4 +316,54 @@
   display: flex
 }
 
+.descriptionFull{
+  display: flex;
+}
+
+.description{
+  width: 85%;
+  text-align: justify;
+  display: flex;
+}
+
+.descriptionImage{
+  width: 25%;
+  text-align: center;
+  margin-left: 10px;
+  margin-right: 10px;
+  display: flex;
+}
+
+.descriptionImageMobile{
+  width: 100%;
+  text-align: justify;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.descriptionImageMobile{
+  width: 100%;
+  text-align: justify;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.links{
+  color: rgb(10, 10, 32);
+}
+
+.linkShow{
+  text-decoration: underline;
+}
+
+.descriptionImageLink{
+  width: 80%;
+  margin-left: 10%;
+  max-width: 1000px;
+  text-align: center;
+}
+
+.overlay{
+  text-align: center;
+}
 </style>
