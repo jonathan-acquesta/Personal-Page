8935916df211a49cd6f2d6832de6983c3e1c8b95
Jonathan Caravaggio Acquesta <jonathan.acquesta@hotmail.com>
2020-06-02 02:20:12 -0300

    new resume pdf

--- personal-page/src/components/Header.vue
+++ personal-page/src/components/Header.vue
@@ -8,6 +8,9 @@
       dense
     >
         <v-btn icon>
+          <v-icon  size="24px" @click="openSite('https://github.com/jonathan-acquesta/')">mdi-file-pdf</v-icon>
+        </v-btn>
+        <v-btn icon class="gitButton">
           <v-icon  size="24px" @click="openSite('https://www.linkedin.com/in/jonathan-caravaggio-acquesta-a26a4422/')">fab fa-linkedin</v-icon>
         </v-btn>
         <v-btn icon class="gitButton">
